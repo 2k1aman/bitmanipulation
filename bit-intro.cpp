03e508120530592e876c36843d660063ae281150
2k1aman <2k1amankumar@gmail.com>
2023-08-30 20:31:44 +0530

    shifts

---
+++ bit-intro.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // right shift left shift
+    int a = 5;
+    int b = a<<2;
+    cout<<b<<"\n";
+    // b = 20
+    // << left shift multiplies by 2
+    cout<< (a>>2);
+    // right shift divided by 2
+    // 1
+    return 0;
+}
